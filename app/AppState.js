46c510393b90d37d4c91e55148e041788acea4d6
Justin Morrow <justintwomorrow@gmail.com>
2025-02-26 15:55:47 -0700

    another one

--- app/AppState.js
+++ app/AppState.js
@@ -65,8 +65,17 @@ class ObservableAppState extends EventEmitter {
       yard: false,
       floors: 1,
       imgUrl: `https://images.unsplash.com/photo-1472224371017-08207f84aaae?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
-
+    }),
+    new House({
+      bedroom: `3`,
+      bathroom: `2`,
+      sqfoot: 1800,
+      price: 385000,
+      yard: false,
+      floors: 1,
+      imgUrl: `https://images.unsplash.com/photo-1472224371017-08207f84aaae?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
     })
+
   ]
 
 }
